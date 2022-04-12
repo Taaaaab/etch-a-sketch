856984722ff9700febb51919deadaaf5a0a1d461
Taaaaab <owen.whitsett@gmail.com>
2022-04-11 21:21:20 -0400

    Create HTML, CSS and Javascript for project

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#container');
+
+const squares = document.createElement('div');
+squares.classList.add('squares');
+squares.textContent = 'This is the glorious text-content!';
+
+container.appendChild(squares);
