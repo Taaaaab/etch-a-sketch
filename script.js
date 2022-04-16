8ccd4b2c0dd18b857a2c770770688d902a9560e0
Taaaaab <owen.whitsett@gmail.com>
2022-04-15 22:30:32 -0400

    Add reset button and ability to input choice again

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 const container = document.querySelector('#container');
 const btn = document.querySelector('#btn');
-const userSquare = Number(window.prompt("Number of Squares per Side?", 16));
-function reset(userSquare) {
-  let i = userSquare;
-  for (i; i <= 100; i++){
-    let newDiv = document.createElement('div');
+const newDiv = document.createElement('div');
+let userSquare = Number(window.prompt("Number of Squares per Side?", 32));
+
+function resetGame(userSquare) {
+    for (let i = 0; i <= userSquare; i++) {
+    const newDiv = document.createElement('div');
     newDiv.setAttribute('id', 'r'+i);
     newDiv.classList.add('squares');
     container.appendChild(newDiv);
     // Hover effect on mouseenter
     newDiv.addEventListener("mouseenter", function( event ) {
-      // highlight the mouseenter target
-      event.target.classList.add('hover');
-      newDiv.style.cssText = 'background-color: black'; 
-    });
-
-    btn.addEventListener('click', () => {
-      newDiv.classList.remove('hover');
-      newDiv.style.cssText = 'background-color: white';                                   
-    });
-    
+    // Highlight the mouseenter target
+    event.target.classList.add('hover'); 
+    }); 
   }
 }
-reset(userSquare);
+resetGame(userSquare);
 document.body.appendChild(container);
 
+btn.addEventListener('click', () => {
+  let resetDiv = document.getElementsByTagName("div");
+[].forEach.call(resetDiv, function(removeHover) {
+  removeHover.classList.remove("hover");
+  removeHover.classList.add("removeHover");
+    function removeAllChildNodes(parent) {
+      while (parent.firstChild) {
+          parent.removeChild(parent.firstChild);
+      }
+    }
+  removeAllChildNodes(container);
+  });
+  
+  let userSquare = Number(window.prompt("Number of Squares per Side?", 32));
+  resetGame(userSquare);
+  document.body.appendChild(container);                           
+});
 
